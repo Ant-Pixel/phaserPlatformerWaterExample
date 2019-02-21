980e575e44971097bd99bc7bb609e3d1cabb5302
e-a-n <ean.director@gmail.com>
2019-02-20 23:26:33 -0800

    added physics in create method and tested physics velocity in update method, preparing to get rid of hard coded values, add in the platforms

--- client/js/level1.js
+++ client/js/level1.js
@@ -8,8 +8,11 @@ level1State.create = () => {
         config.level1State.player.spriteKey
      ];
     let player = game.add.sprite(...playerData);
+    game.physics.arcade.enable(player);
+
+    level1State.player = player;
 };
 
 level1State.update = () => {
-
+    level1State.player.body.velocity.x = 200;
 };
