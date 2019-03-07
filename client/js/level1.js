16d1ad5a0f86df3d83dbfcf5cf06211dba17d2d6
Jansen Henschel <jansensblog2012@gmail.com>
2019-03-06 22:17:10 -0800

    Turned waterBubbles into an object with three functions: init, create, and update. create is generally the same code I had in the last commit, with a couple of tweaks to prevent global variables and three new parameters. These parameters are emitX, which controls the x position that the water bubbles emit from. EmitY, which is the same as emitX but for the y position. And bubblesDisappearY, which controls where the bubble will disappear. The update function attempts to update the emitX and emitY variables of the emitter. Currently, no bubbles are coming out of the emitter.

--- client/js/level1.js
+++ client/js/level1.js
@@ -1,5 +1,9 @@
 const level1State = {};
 
+level1State.init = () => {
+    waterBubbles.init(game);
+}
+
 level1State.create = () => {
     console.log("We are in the create state!!!");
     let playerData = [
@@ -8,11 +12,16 @@ level1State.create = () => {
         config.level1State.player.spriteKey
      ];
     let player = game.add.sprite(...playerData);
+    game.physics.startSystem(Phaser.Physics.ARCADE);
     game.physics.arcade.enable(player);
 
     level1State.player = player;
+    waterBubbles.create(level1State.player.body.x, level1State.player.body.y, 0);
 };
 
 level1State.update = () => {
-    level1State.player.body.velocity.x = 200;
+    level1State.player.body.velocity.x = 10;
+    //console.log(level1State.player.body.x);
+    //console.log(level1State.player.body.y);
+    waterBubbles.update(level1State.player.body.x, level1State.player.body.y);
 };
--- client/js/waterBubbles.js
+++ client/js/waterBubbles.js
@@ -1,5 +1,17 @@
-function waterBubbles() {
-	var emitter = Phaser.add.emitter(game, config.default.waterBubbles.emitX, config.default.waterBubbles.emitY, config.default.waterBubbles.maxParticles);
+let waterBubbles = {};
+
+waterBubbles.init = (game) => {
+	waterBubbles.game = game;
+}
+
+waterBubbles.create = (emitX, emitY, bubblesDisappearY) => {
+	let game = waterBubbles.game;
+	let bubbleTravelDistance = bubblesDisappearY - emitY;
+	let bubbleTravelTime = bubbleTravelDistance/config.default.waterBubbles.particleSpeed.minParticleYSpeed;
+	let lifespan = bubbleTravelTime*1000;
+	let alphaEasingRate = bubbleTravelTime*1000;
+
+	let emitter = game.add.emitter(game, emitX, emitY, config.default.waterBubbles.maxParticles);
 	emitter.makeParticles(config.default.waterBubbles.spriteKey);
 	
 	emitter.gravity = config.default.waterBubbles.yGravity;
@@ -10,9 +22,17 @@ function waterBubbles() {
 	let scaleData = [config.default.waterBubbles.particleScale.minParticleXScale, config.default.waterBubbles.particleScale.maxParticleXScale, config.default.waterBubbles.particleScale.minParticleYScale, config.default.waterBubbles.particleScale.maxParticleYScale, config.default.waterBubbles.particleScale.scaleEasingRate, config.default.waterBubbles.particleScale.scaleEasingFormula, config.default.waterBubbles.particleScale.scaleYoyo];
 	emitter.setScale(...scaleData);
 
-	let alphaData = [config.default.waterBubbles.particleAlpha.minAlpha, config.default.waterBubbles.particleAlpha.maxAlpha, config.default.waterBubbles.particleAlpha.alphaEasingRate, config.default.waterBubbles.particleAlpha.alphaEasingFormula, config.default.waterBubbles.particleAlpha.alphaYoyo];
+	let alphaData = [config.default.waterBubbles.particleAlpha.minAlpha, config.default.waterBubbles.particleAlpha.maxAlpha, alphaEasingRate, config.default.waterBubbles.particleAlpha.alphaEasingFormula, config.default.waterBubbles.particleAlpha.alphaYoyo];
 	emitter.setAlpha(...alphaData);
 
-	let generalEmitterData = [config.default.waterBubbles.explode, config.default.waterBubbles.lifespan, config.default.waterBubbles.frequency, config.default.waterBubbles.quantity, config.default.waterBubbles.forceQuantity];
-	emitter.start(...generalEmitterData);
+	let generalEmitterData = [config.default.waterBubbles.explode, lifespan, config.default.waterBubbles.frequency, config.default.waterBubbles.quantity, config.default.waterBubbles.forceQuantity];
+	waterBubbles.emitter = emitter.start(...generalEmitterData);
+	console.log(emitter);
+}
+
+waterBubbles.update = (emitX, emitY) => {
+	waterBubbles.emitter.emitX = emitX;
+	waterBubbles.emitter.emitY = emitY;
+	waterBubbles.emitter.makeParticles(config.default.waterBubbles.spriteKey);
+	console.log(waterBubbles.emitter);
 }
